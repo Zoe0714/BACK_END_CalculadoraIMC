2be11eae2e3b70cf6d5438fd3fc93db877e9132d
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-03-13 08:09:08 -0300

    Add files via upload

--- IMC.js
+++ IMC.js
@@ -1,35 +1,31 @@
-const calcular = document.createElement("calcular");
-function imc() {
-  const nome = document.getElementById("nome").value;
-  const altura = document.getElementById("altura").value;
-  const peso = document.getElementById("peso").value;
-  const resultado = document.getElementById("resultado");
+const calcular = document.getElementById('calcular');
+function imc(){
+  const nome = document.getElementById('nome').value;
+  const altura = document.getElementById('altura').value;
+  const peso = document.getElementById('peso').value;
+  const resultado = document.getElementById('resultado');
 
   if(nome !=='' && altura !=='' && peso !==''){
 
-    const valorIMC = (peso /(altura * altura)).toFixed(1);
+    const valorIMC =(peso /(altura * altura)).toFixed(1);
 
-    let classificacao ='';
+    let classificacao = '';
     if(valorIMC < 18.5){
       classificacao = 'abaixo do peso';
     }
-    else if(valorIMC > 25){
+    else if(valorIMC < 25){
       classificacao = 'com peso normal';
-    }
-    else if(valorIMC > 30){
+    }else if(valorIMC < 30){
       classificacao = 'levemente acima do peso';
-    }
-    else if(valorIMC > 35){
+    }else if(valorIMC < 35){
       classificacao = 'com obesidade grau 1';
-    }
-    else if(valorIMC > 40){
-      classificacao = 'com obesidade grau 2';
-    }
-    else{
-      classificacao = 'com obesidade grau 3. Cuidado!';
-    }
-    resultado.textContent = `${nome}, seu IMC é ${valorIMC} e você está ${classificacao}`
 
+    }else if(valorIMC <40){
+      classificacao = 'com obesidade grau 2';
+    }else{
+      classificacao = 'com obesidade grau 3. Cuidado!!';
+    }
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
   }
   else{
     resultado.textContent = 'Preencha todos os campos';
